264da8dfb1f7871a8e7de94abde1ad9f1ca6dc2a
taliento <davidetaliento@gmail.com>
2016-01-20 15:51:15 +0100

    message

---
+++ app/components/rental-listing.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+
+export default Ember.Component.extend({
+
+	  isImageShowing: false,
+			   actions: {
+					     imageShow() {
+								       this.set('isImageShowing', true);
+											     },
+			     imageHide() {
+						       this.set('isImageShowing', false);
+	    }
+  }
+});
